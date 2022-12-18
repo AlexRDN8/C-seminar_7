353da69b42b44c6964f65f0144f8dd21906320ad
AlexRDN8 <alexrodin8@bk.ru>
2022-12-18 11:31:19 +0300

    added info about string metods

---
+++ example2/Program.cs
@@ -0,0 +1,14 @@
+﻿//// Тема 3: Строки. 
+// Метод Join:
+int [] array = new int [3]{1,3,4};
+string str = string.Join("--", array);
+Console.WriteLine(str);
+
+// Метод Format (заменяет интерполяцию $):
+string str2 = string.Format ("{0} равно {1} - это {2}", "1+1", 4, false);
+Console.WriteLine(str2);
+
+//Метод Concat (складывает строчки):
+string str3 = string.Concat("-","+","=");
+Console.WriteLine(str3);
+
